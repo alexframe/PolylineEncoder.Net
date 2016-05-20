e33e920f6ce540b1d2fe4a9ddd9a21696d6f1adf
alex.frame <alex.frame@domain.com.au>
2016-05-20 12:09:35 +1000

    Test Xunit with AppVeyor.

---
+++ test/PolylineEncoder.Net.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PolylineEncoder.Net.Tests
+{
+
+    public class Class1
+    {
+        [Fact]
+        public void TestXUnitDnx()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
